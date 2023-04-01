4933684499bbec91f71d9dd97961c2b6e313a75f
Leandro Santana <santana.turbo@gmail.com>
2023-03-31 23:30:13 -0300

    Biblioteca instalada

--- db/migrate/20230401012229_create_heroes.rb
+++ db/migrate/20230401012229_create_heroes.rb
@@ -1,7 +1,7 @@
 class CreateHeroes < ActiveRecord::Migration[7.0]
   def change
     create_table :heroes do |t|
-      t.string :name, :null => false
+      t.string :name, null: false
       
 
       t.timestamps
